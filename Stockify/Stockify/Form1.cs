aa9e40f6b3cf0e6beb443c483eedb17eaa76d757
TJ Maynes <tjmaynes@outlook.com>
2015-03-05 10:54:11 -0500

    Update Form1.cs

--- Stockify/Stockify/Form1.cs
+++ Stockify/Stockify/Form1.cs
@@ -42,7 +42,18 @@ namespace Stockify
         /// </summary>
         private void dtpStartDate_ValueChanged(object sender, EventArgs e)
         {
-
+            try
+            {
+                 //Start Date
+                 //User has selected a starting date
+                 Query.startDay = dtpStartDate.Value.Day.ToString();
+                 Query.startMonth = dtpStartDate.Value.Month.ToString();
+                 Query.startYear = dtpStartDate.Value.Year.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Error: dtpStartDateFS_ValueChanged method");
+            }
         }
 
         /// <summary>
@@ -50,15 +61,39 @@ namespace Stockify
         /// </summary>
         private void dtpEndDate_ValueChanged(object sender, EventArgs e)
         {
-
-        }
+            try
+            {
+                //Ending Date
+                //User has selected a Ending date
+                Query.endDay = dtpEndDate.Value.Day.ToString();
+                Query.endMonth = dtpEndDate.Value.Month.ToString();
+                Query.endYear = dtpEndDate.Value.Year.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Error: dtpEndDate_ValueChanged method");
+            }
+       }
 
         /// <summary>
         /// Resolution -> User Picked Daily
         /// </summary>
         private void rbDaily_CheckedChanged(object sender, EventArgs e)
         {
-
+            try
+            {
+                if (rbDaily.Checked == true)
+                {
+                    Query.isDaily = true;
+                    Query.isWeekly = false;
+                    Query.isMonthly = false;
+                    fileLocation = "Daily";
+                }
+            }
+            catch 
+            {
+                MessageBox.Show("Error: rbDaily_CheckedChanged");
+            }
         }
 
         /// <summary>
@@ -66,7 +101,20 @@ namespace Stockify
         /// </summary>
         private void rbWeekly_CheckedChanged(object sender, EventArgs e)
         {
-
+            try
+            {
+                if (rbWeekly.Checked == true)
+                {
+                    Query.isDaily = false;
+                    Query.isWeekly = true;
+                    Query.isMonthly = false;
+                    fileLocation = "Weekly";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error: rbWeekly_CheckedChanged");
+            }
         }
 
         /// <summary>
@@ -74,7 +122,20 @@ namespace Stockify
         /// </summary>
         private void rbMonthly_CheckedChanged(object sender, EventArgs e)
         {
-
+            try
+            {
+                if (rbWeekly.Checked == true)
+                {
+                    Query.isDaily = false;
+                    Query.isWeekly = false;
+                    Query.isMonthly = true;
+                    fileLocation = "Monthly";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error: rbMonthly_CheckedChanged");
+            }
         }
 
         /// <summary>
@@ -82,7 +143,20 @@ namespace Stockify
         /// </summary>
         private void rbOnline_CheckedChanged(object sender, EventArgs e)
         {
-
+            try
+            {
+                if (rbOnline.Checked == true)
+                {
+                    loadMethod = "Online";
+                    btnSubmit.Visible = true;
+                    btnSubmit.Text = "Online";
+                    rbDisplay.Checked = false;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error: rbOnline_CheckedChanged");
+            }
         }
 
         /// <summary>
@@ -90,7 +164,20 @@ namespace Stockify
         /// </summary>
         private void rbDisplay_CheckedChanged(object sender, EventArgs e)
         {
-
+            try
+            {
+                if (rbDisplay.Checked == true)
+                {
+                    loadMethod = "Offline";
+                    btnSubmit.Visible = true;
+                    btnSubmit.Text = "Display";
+                    rbOnline.Checked = false;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error: rbDisplay_CheckedChanged");
+            }
         }
 
         /// <summary>
@@ -98,7 +185,32 @@ namespace Stockify
         /// </summary>
         private void btnClear_Click(object sender, EventArgs e)
         {
+            //Clear the fields of the form
+            txtCompanyTicker.Clear();
+            rbDaily.Checked = false;
+            rbWeekly.Checked = false;
+            rbMonthly.Checked = false;
 
+            rbOnline.Checked = false;
+            rbDisplay.Checked = false;
+
+            //Clear the query
+            Query.startDay = "";
+            Query.startMonth = "";
+            Query.startYear = "";
+
+            Query.endDay = "";
+            Query.endMonth = "";
+            Query.endYear = "";
+
+            Query.isDaily = false;
+            Query.isMonthly = false;
+            Query.isWeekly = false;
+
+            Query.companyName = "";
+           
+            //Hide submit button
+            btnSubmit.Visible = false;
         }
 
         /// <summary>
@@ -106,7 +218,45 @@ namespace Stockify
         /// </summary>
         private void btnSubmit_Click(object sender, EventArgs e)
         {
+            try
+            {
+                //check for end date is not less than start date
+                int checkDay = int.Parse(Query.endDay) - int.Parse(Query.startDay);
+                int checkMonth = int.Parse(Query.endMonth) - int.Parse(Query.startMonth);
+                int checkYear = int.Parse(Query.endYear) - int.Parse(Query.startYear);
 
+                if ((checkDay >= 0) && (checkMonth >= 0) && (checkYear >= 0) )
+                {
+                    //If date is valid, then continue
+                    string filePath;
+                    string currentLine = "";
+                    Query.companyName = txtCompanyTicker.Text.Trim();
+
+                    if (rbOnline.Checked == true)
+                    {
+                        //Assemble the search query - Online
+                        QueryAssembly.buildStockQuery(Query);
+                        
+                        //Read saved file
+                        filePath = QueryFile.readFromFile(fileLocation, Query.companyName);
+
+                        //read saved file
+                        var readCSV = (inputFile.parseCSV(filePath)).ToString();
+                        MessageBox.Show(readCSV);
+                        
+                        
+                        
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error: Sorry the end date select is before the selected start date");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: btnSubmit_Click: "+ ex.Message);
+            }
         }
     }
 }
